b66a8be9c94abae9092f967da227790484949ba5
MaxAn26 <136971232+MaxAn26@users.noreply.github.com>
2025-05-11 17:25:34 +0300

    Base core

---
+++ BaseMod.Core/Utils/RandomUtils.cs
@@ -0,0 +1,65 @@
+﻿namespace BaseMod.Core.Utils;
+public static class RandomUtils {
+    public static Random DefaultRandom { get; } = Random.Shared;
+
+    public static bool Chance( int chance ) => Chance( chance, true, false );
+    public static T Chance<T>( int chance, T successValue, T defaultValue ) {
+        if(chance < 0 || chance > 100)
+            throw new ArgumentOutOfRangeException( nameof( chance ), $"Value '{nameof( chance )}' should be in range [0-100]" );
+
+        double dblChance = chance / 100f;
+        return Chance( dblChance, successValue, defaultValue );
+        /*if(chance == 0)
+            return defaultValue;
+
+        if(chance == 100)
+            return successValue;
+
+        int rnd = Int32( 0, 100 );
+        return chance >= rnd ? successValue : defaultValue;*/
+    }
+
+    public static bool Chance( double chance ) => Chance( chance, true, false );
+
+    public static T Chance<T>( double chance, T successValue, T defaultValue ) {
+        if(chance < 0 || chance > 100)
+            throw new ArgumentOutOfRangeException( nameof( chance ), $"Value '{nameof( chance )}' should be in range [0-100]" );
+
+        if(chance == 0)
+            return defaultValue;
+
+        if(chance == 1)
+            return successValue;
+
+        double rnd = DefaultRandom.NextDouble();
+        return chance >= rnd ? successValue : defaultValue;
+    }
+
+    public static int Int32( int max ) => Int32( 0, max );
+
+    public static int Int32( int min, int max ) => DefaultRandom.Next( min, max + 1 );
+
+    public static double Double( double max ) {
+        return Double( 0.0, max );
+    }
+
+    public static double Double( double min, double max ) {
+        return DefaultRandom.NextDouble() * (max - min) + min;
+    }
+
+    public static float Float( float min, float max ) {
+        return DefaultRandom.NextSingle() * (max - min) + min;
+    }
+
+    public static T Item<T>( IEnumerable<T> items ) => Item( items.ToList() );
+
+    public static T Item<T>( IList<T> items ) {
+        if(items.Count == 0)
+            throw new ArgumentException( "Collection should contain at least 1 item" );
+
+        if(items.Count == 1)
+            return items[0];
+
+        return items[DefaultRandom.Next( items.Count )];
+    }
+}
